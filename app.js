97b08d2f9cebcd9c456f9971368a42d66ab9b847
Behzod <bmoydinboyev@usf.edu>
2021-08-02 18:01:57 +0500

    Secure API keys

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const https = require ("https");
 const bodyParser = require("body-parser");
 
+//Security
+require("dotenv").config();
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -18,8 +20,8 @@ app.post("/", function(req, res) {
         return true;
     }
 
-    var degreeUnit = "metric"
-    const APIKey = "40eac2c4a9ad8a312a59e46f4867c804";
+    var degreeUnit = "metric";
+    const APIKey = process.env.OPEN_WEATHER_API_KEY;
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + degreeUnit + "&appid=" + APIKey;
 
     https.get(url, function (response) {
@@ -40,7 +42,7 @@ app.post("/", function(req, res) {
             const cloudNumber = weatherData.clouds.all;
             const longitude = weatherData.coord.lon;
             const latitude = weatherData.coord.lat;
-            const weatherIconURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+            const weatherIconURL = "//openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
 
             res.json({temperature, humidity, windSpeed, country, cityName, weatherDescription, pressure, dateTime, weatherIconURL, cloudNumber, latitude, longitude});
         })
